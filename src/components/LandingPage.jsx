9df823463f171720b531a8fdc381c1bae0934a96
Harsh <harshb15003@gmail.com>
2023-09-17 16:30:47 +0530

    whatever

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 const LandingPage = () => {
   return (
@@ -15,9 +15,9 @@ const LandingPage = () => {
       
 
       {/* Call to Action */}
-      <section className="bg-blue-500 py-12">
-        <div className="container mx-auto text-center">
+      <section className="bg-blue-500 py-12 text-center">
           <h2 className="text-2xl font-semibold text-white mb-4">Get Started</h2>
+        <div className=" text-center main-container mx-3 gap-3 grid place-content-center">
           <br></br>
           <a href="/guard-login" className="bg-white text-blue-500 hover:bg-blue-400 mx-8 py-2 px-6 rounded-full text-lg font-semibold transition duration-300">
             Guard Login
