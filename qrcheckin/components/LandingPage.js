bddba95d684514c38fd1d32eae6a5dbba89572a0
Akshat Gupta <akshatgtc@gmail.com>
2023-09-17 16:23:57 +0530

    intialised

---
+++ qrcheckin/components/LandingPage.js
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+
+const LandingPage = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen flex flex-col">
+      {/* Header */}
+      <header className="bg-blue-500 py-4">
+        <div className="container mx-auto text-white text-center">
+          <h1 className="text-3xl font-semibold">QR Check-In</h1>
+          <p className="mt-2">Streamline hostel entry with QR codes.</p>
+        </div>
+      </header>
+
+      {/* Call to Action */}
+      <section className="bg-blue-500 py-12">
+        <div className="container mx-auto text-center">
+          <h2 className="text-2xl font-semibold text-white mb-4">Get Started</h2>
+          <br />
+          <Link href="/login">
+            <button className="bg-white text-blue-500 hover:bg-blue-400 mx-8 py-2 px-6 rounded-full text-lg font-semibold transition duration-300">
+              Guard Login
+            </button>
+          </Link>
+          <Link href="/login">
+            <button className="bg-white text-blue-500 hover:bg-blue-400 mx-8 py-2 px-6 rounded-full text-lg font-semibold transition duration-300">
+              Student Login
+            </button>
+          </Link>
+          <Link href="/login">
+            <button className="bg-white text-blue-500 hover:bg-blue-400 mx-8 py-2 px-6 rounded-full text-lg font-semibold transition duration-300">
+              Authority Login
+            </button>
+          </Link>
+        </div>
+      </section>
+
+      {/* Footer */}
+      <footer className="bg-gray-200 py-4">
+        <div className="container mx-auto text-center">
+          <p>&copy; {new Date().getFullYear()} QR Check-In</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default LandingPage;
